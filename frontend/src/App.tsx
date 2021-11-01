fa4997c3b01b18721274f378a7be2b0991cccb04
Nilton Santana <tonssantos@gmail.com>
2021-11-01 18:05:52 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-function App() { 
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1> 
-    </div>
+    <>
+      <NavBar />
+      <div className="container"> 
+        <h1 className="text-primary">Olá mundo!</h1>
+      </div>
+      <Footer /> 
+    </>
   );
-} 
+}
 
 export default App;
